8f16a5381fd18deff8cd8913101e00e9388c80f7
Atos Vinicius <athos.vinicius.pereira@gmail.com>
2022-10-10 20:22:03 -0300

    ajuste Atos

--- script.js
+++ script.js
@@ -1,71 +1,79 @@
 var listaderegistros = [];
+
 function add() {
-    
-    var nomefuncionario = document.getElementById("nome").value;
-    var funcao = document.getElementById("funcao").value;
-    var salario = document.getElementById("salario").value;
+  var nomefuncionario = document.getElementById("nome").value;
+  var funcao = document.getElementById("funcao").value;
+  var salario = document.getElementById("salario").value;
 
-    var registro = { 
-        nomefuncionario : nomefuncionario,
-        funcao : funcao,  
-        salario : salario
-    }
-    listaderegistros.push(registro);
-    listar(registro)
-    
-    var formulario = document.getElementById('formulario');
-    formulario.reset();
+  var registro = {
+    nomefuncionario: nomefuncionario,
+    funcao: funcao,
+    salario: salario,
+  };
+  listaderegistros.push(registro);
+
+  resetarFormulario();
+  limparHtml();
+  listar();
 }
 
-function listar(registro) {
-
-    var dados = document.getElementById('dados');
-    var elementoLi = document.createElement('li');
-    elementoLi.setAttribute("id", "dadoslistaNome")
+function listar() {
+  listaderegistros.forEach((element) => {
+    const registro = element;
+    var dados = document.getElementById("lista");
+    var elementoLi = document.createElement("li");
+    elementoLi.setAttribute("id", "dadoslistaNome");
     elementoLi.append(registro.nomefuncionario);
+
+    const botaoEditar = document.createElement("button");
+    botaoEditar.innerHTML = "Editar";
+
+    const botaoDeletar = document.createElement("button");
+    botaoDeletar.innerHTML = "Deletar";
+    botaoDeletar.onclick = () => {
+      excluir(elementoLi);
+    };
+
+    elementoLi.append(botaoEditar);
+    elementoLi.append(botaoDeletar);
+
     dados.append(elementoLi);
-
-    var dadosF = document.getElementById('dadosF');
-    var elementoLi = document.createElement('li');
-    elementoLi.setAttribute("id", "dadoslistaFuncao")
-    elementoLi.append(registro.funcao);
-    dadosF.append(elementoLi);
-
-    var dados = document.getElementById('dadosS');
-    var elementoLi = document.createElement('li');
-    elementoLi.setAttribute("id", "dadoslistaSalario")
-    elementoLi.append(registro.salario);
-    dadosS.append(elementoLi);
-
+  });
 }
 
-function excluir() {
-    
-    document.getElementById('dadoslistaNome').remove()
-    document.getElementById('dadoslistaFuncao').remove()
-    document.getElementById('dadoslistaSalario').remove()
-  
+function resetarFormulario() {
+  var formulario = document.getElementById("formulario");
+  formulario.reset();
+}
+
+function limparHtml() {
+  var lista = document.getElementById("lista");
+  lista.innerHTML = "";
+}
+
+function excluir(registro) {
+  registro.remove();
 }
 
 function editar(id) {
-    document.getElementById(id).style.display = 'block';
+  document.getElementById(id).style.display = "block";
 }
+
 function fechar(id) {
-    document.getElementById(id).style.display = 'none';
+  document.getElementById(id).style.display = "none";
 }
 
 function editname() {
-    var newname = window.prompt('Digite o novo nome:')
-    document.getElementById('dadoslistaNome').innerHTML = newname;
-    
+  var newname = window.prompt("Digite o novo nome:");
+  document.getElementById("dadoslistaNome").innerHTML = newname;
 }
 
 function editfunction() {
-    var newfunction = window.prompt('Digite a nova função:')
-    document.getElementById('dadoslistaFuncao').innerHTML = newfunction;
+  var newfunction = window.prompt("Digite a nova função:");
+  document.getElementById("dadoslistaFuncao").innerHTML = newfunction;
 }
 
 function editsalary() {
-    var newsalary = window.prompt('Digite o novo salario:')
-     document.getElementById('dadoslistaSalario').innerHTML = newsalary;
-}  
+  var newsalary = window.prompt("Digite o novo salario:");
+  document.getElementById("dadoslistaSalario").innerHTML = newsalary;
+}
